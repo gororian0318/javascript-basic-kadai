6b8751afb03c922de51e20549206186bfb2491b4
Kadota Hiroki <167946186+gororian0318@users.noreply.github.com>
2025-04-27 22:20:24 +0900

    課題20提出

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const clickBtn = document.getElementById('btn');
+
+clickBtn.addEventListener('click', () => {
+  const clickText = document.getElementById('text');
+  clickText.textContent = 'ボタンをクリックしました';
+});
